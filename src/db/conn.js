484970922eac3edc667396cd8539759224182e7c
Dhanuvnayak <dhanalakshmi.v@centelon.com>
2022-11-07 12:41:47 +0530

    adding script for heroku

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://dhanalakshmi:dhanu@cluster0.sf5fqyd.mongodb.net/?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://dhanalakshmi:dhanu@cluster0.8c0srz0.mongodb.net/?retryWrites=true&w=majority", {
     useNewUrlParser:true,
     useUnifiedTopology:true,
     useCreateIndex:true
